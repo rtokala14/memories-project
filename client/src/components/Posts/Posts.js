314a4b39741eb178309127e425df2952aa8d0dbd
Rohit Tokala <rtokala@purdue.edu>
2022-07-21 00:07:04 +0400

    Display posts on homepage as cards

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Post from "./Post/Post";
 
+import { Grid, CircularProgress } from "@material-ui/core";
+
 import { useSelector } from "react-redux";
 
 import useStyles from "./styles";
@@ -10,12 +12,21 @@ const Posts = () => {
   const posts = useSelector((state) => state.posts);
 
   console.log(posts);
-  return (
-    <>
-      <h1> Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
