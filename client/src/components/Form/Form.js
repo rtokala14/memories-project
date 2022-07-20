249a2d6ec1d48e151364208e8ecd77f694d8686a
Rohit Tokala <rtokala@purdue.edu>
2022-07-20 23:45:57 +0400

    Add post to cloud data

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -4,6 +4,9 @@ import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
 
+import { useDispatch } from "react-redux";
+import { createPost } from "../../actions/posts";
+
 const Form = () => {
   const [postData, setPostData] = useState({
     creator: "",
@@ -14,7 +17,13 @@ const Form = () => {
   });
   const classes = useStyles();
 
-  const handleSubmit = () => {};
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData));
+  };
 
   const clear = () => {};
 
