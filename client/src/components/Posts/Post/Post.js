488e3fd3f35b24fe0e78fe29068f5ec59388151e
Rohit Tokala <rtokala@purdue.edu>
2022-07-21 01:12:00 +0400

    Implement delete post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -12,11 +12,15 @@ import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../actions/posts";
+
 import moment from "moment";
 
 import useStyles from "./styles";
 
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch();
   const classes = useStyles();
   return (
     <Card className={classes.card}>
@@ -61,7 +65,11 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -43,3 +43,14 @@ export const updatePost = async (req, res) => {
 
   res.json(updatePost);
 };
+
+export const deletePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that id");
+
+  await postMessage.findByIdAndRemove(_id);
+
+  res.json({ message: "Post deleted successfully" });
+};
